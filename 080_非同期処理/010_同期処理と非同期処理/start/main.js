44da5b4ea5080e240c587cb96e514fa0a9526b76
komaitaira <komaitaira@gmail.com>
2021-01-13 16:14:47 +0900

    finish タスクキューとコールスタック

--- 080_非同期処理/010_同期処理と非同期処理/start/main.js
+++ 080_非同期処理/010_同期処理と非同期処理/start/main.js
@@ -8,3 +8,8 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', function(){
   console.log('button clicked');
 });
+
+// 同期処理では、メインスレッドでコードが順番に実行される。
+// 一つの処理が完了するまで次の処理に進まない
+
+// 非同期処理では、一時的にメインスレッドから処理が切り離される
--- 080_非同期処理/020_タスクキューとコールスタック/start/main.js
+++ 080_非同期処理/020_タスクキューとコールスタック/start/main.js
@@ -1,3 +1,9 @@
+// 【タスクキュー】
+// キュー：　実行待ちの非同期処理の行列のこと
+// タスクキュー：　非同期処理の実行順を管理しているキュー
+// メインスレッドが占有されている状態：　コールスタックにコンテキストが積まれている状態
+
+
 const btn = document.querySelector('button');
 btn.addEventListener('click', function task2() {
     console.log('task2 done');
@@ -13,3 +19,5 @@ function a() {
 
     console.log('fn a done');
 }
+
+a();
