4bcdc7b17fee171c59322a1afeffb86090a3136d
komaitaira <komaitaira@gmail.com>
2021-01-13 16:35:15 +0900

    finish 030コールバック関数と非同期処理

--- 080_非同期処理/030_コールバック関数と非同期処理/start/main.js
+++ 080_非同期処理/030_コールバック関数と非同期処理/start/main.js
@@ -1,6 +1,30 @@
-function a() {
-  setTimeout(function task1() { 
-    console.log('task1 done');
+// function a() {
+//   setTimeout(function task1() { //②関数aからsetTimeout(非同期API)が呼ばれtask1がタスクキューに登録される
+//     console.log('task1 done');
+//   });
+
+//   console.log('fn a done');
+// }
+
+// function b() {
+//   console.log('fn b done');
+// }
+
+// a(); //①まずグローバルコンテキストから関数aが実行される（コールスタックに積まれている）
+
+// b(); //③関数bがコールスタックに積まれる（ここで、関数bがコールスタックにあるためタスクキューにあるtask1は、コールスタックに空きがないと実行されない）
+
+// したがって上記の書き方になると、実行される順番として
+// 1. fn a done
+// 2. fn b done
+// 3. fn task1 done
+// という順番になる。
+// fn b の表示をtask1より遅くするためには、
+
+function a(b) {
+  setTimeout(function task1() { //②関数aからsetTimeout(非同期API)が呼ばれtask1がタスクキューに登録される
+    console.log('task1 done'); //この時、setTimeoutの実行時に関数bの実行を一緒にするようにしてあげると、task1 doneの後にfn bが実行されるようになる
+    b();
   });
 
   console.log('fn a done');
@@ -10,6 +34,5 @@ function b() {
   console.log('fn b done');
 }
 
-a();
+a(b); //①まずグローバルコンテキストから関数aが実行される（コールスタックに積まれている）
 
-b();
