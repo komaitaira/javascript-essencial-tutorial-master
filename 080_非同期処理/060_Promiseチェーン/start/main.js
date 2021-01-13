f845396887b081ab6f09c73c94196be7d60845a6
komaitaira <komaitaira@gmail.com>
2021-01-13 17:44:29 +0900

    finish 060Promiseチェーン

--- 080_非同期処理/060_Promiseチェーン/start/main.js
+++ 080_非同期処理/060_Promiseチェーン/start/main.js
@@ -1,6 +1,21 @@
-function sleep(callback, val) {
-  setTimeout(function() {
-    console.log(val++);
-    callback(val);
-  }, 1000);
+// 【Promiseチェーン】
+// Promiseを使って非同期処理を順次実行すること
+
+function sleep(val) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log(val++);
+      resolve(val);
+    }, 1000);
+  });
 }
+
+sleep(0).then(function (val) {
+  return sleep(val); // thenメソッドの中でPromiseチェーンをつなぐ場合には、必ずコールバックの戻り値にPromiseのインスタンスを返すようにする
+}).then(function (val) {
+  return sleep(val);
+}).then(function (val) {
+  return sleep(val);
+}).then(function (val) {
+  return sleep(val);
+})
