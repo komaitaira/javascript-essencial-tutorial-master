97ea573caa2080733481febbcdb4ac58de73ed7f
komaitaira <komaitaira@gmail.com>
2021-01-13 19:33:42 +0900

    finish 100 AwaitとAsync

--- 080_非同期処理/100_AwaitとAsync/start/main.js
+++ 080_非同期処理/100_AwaitとAsync/start/main.js
@@ -1,20 +1,38 @@
+// 【Async】
+// Promiseを返却する関数の宣言を行う。
+
+// 【Await】
+// Promiseを返却する関数の非同期処理が完了するまで待つ
+
+
 function sleep(val) {
   return new Promise(function(resolve) {
-    setTimeout(function() {
+    setTimeout(function () {
       console.log(val++);
       resolve(val);
     }, 1000);
   });
 }
 
-sleep(0).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-})
+async function init() {
+  let val = await sleep(0); //valに代入するのは、sleep関数から値が戻ってきたらとなる
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  console.log(val);
+}
+
+init();
+
+// sleep(0).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// })
