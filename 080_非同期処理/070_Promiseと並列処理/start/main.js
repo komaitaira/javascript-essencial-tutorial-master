c8433928953ad364b5ab664cc42a7fb94a940c6d
komaitaira <komaitaira@gmail.com>
2021-01-13 18:21:08 +0900

    080 MacrotasksとMicrotasks

--- 080_非同期処理/070_Promiseと並列処理/start/main.js
+++ 080_非同期処理/070_Promiseと並列処理/start/main.js
@@ -7,6 +7,10 @@ function sleep(val) {
   });
 }
 
+Promise.all([sleep(2), sleep(3), sleep(4)]).then(function () {
+  console.log('end'); //反復可能オブジェクトが全て完了した時にthenメソッドが呼ばれるので、最後にendが表示される
+});
+
 sleep(0).then(function(val) {
   return sleep(val);
 }).then(function(val) {
--- 080_非同期処理/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
+++ 080_非同期処理/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
@@ -1,7 +1,14 @@
+// 【マクロタスク】
+// これまでのレクチャーでタスクキューと呼んでいた物
+// setTimeout, setInterval, requestAnimationFrameなど
+
 setTimeout(function task1() {
   console.log('task1');
 });
 
+// 【マイクロタスク】ジョブキュー
+// タスクキューとは別で存在する非同期処理の待ち行列
+// Promises, queueMicrotask, MutationObserverなど
 new Promise(function promise(resolve) {
   console.log('promise');
   resolve();
@@ -10,3 +17,6 @@ new Promise(function promise(resolve) {
 });
 
 console.log('global end');
+
+// マクロタスクとマイクロタスクが同じイベントのループで発見された時には、
+// 必ずマイクロタスクから処理が実行される
