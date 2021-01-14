d33da36e18567d71ed44cd8fe28be051f272ceab
komaitaira <komaitaira@gmail.com>
2021-01-14 23:54:24 +0900

    finish 010Proxy

--- 100_様々なオブジェクト/010_Proxy/start/main.js
+++ 100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,1 +1,29 @@
+// 【Proxy】
+// プロパティの操作に独自の処理を追加するためのオブジェクト
+
 const targetObj = { a: 0 };
+const handler = {
+  set: function (target, prop, value, receiver) {
+    console.log(`[set]: ${prop}`);
+    // target[prop] = value;
+    throw new Error('cannnot add prop.');
+  },
+  get: function (target, prop, receiver) {
+    if (target.hasOwnProperty(prop)) {
+      return target[prop];
+    } else {
+      return '-1';
+    }
+    console.log(`[get]: ${prop}`);
+  },
+  deleteProperty: function (target, prop) {
+    console.log(`[delete]: ${prop}`);
+    delete target[prop];
+  }
+}
+
+const pxy = new Proxy(targetObj, handler);
+// pxy.a = 1;
+console.log(pxy.b);
+console.log(pxy.a);
+delete pxy.a;
