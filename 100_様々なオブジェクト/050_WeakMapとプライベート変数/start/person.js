f73700ad1ca97107c0ac247c0aadd573a69cb6bc
komaitaira <komaitaira@gmail.com>
2021-01-15 18:22:58 +0900

    finish 050 WeakMapとプライベート変数

--- 100_様々なオブジェクト/050_WeakMapとプライベート変数/start/person.js
+++ 100_様々なオブジェクト/050_WeakMapとプライベート変数/start/person.js
@@ -1,9 +1,14 @@
+const wm = new WeakMap(); //wmはexportされていないので外部からはアクセスできない
+
 export class Person {
     constructor(name, age) {
-        this._name = name;
+        // this._name = name; //「_(アンダースコア)」から始まる変数は、プライベート変数、またはプライベートプロパティと呼び、外部から使用しないように、ということを明示的に示しているもの。
+        wm.set(this, {
+            name
+        })
     }
 
     hello() {
-        console.log(`hello ${this._name}`);
+        console.log(`hello ${wm.get(this).name}`);
     }
 }
