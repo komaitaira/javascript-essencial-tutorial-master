2115cfdd10a3be3096b522044bf3c5bf6ab081ec
komaitaira <komaitaira@gmail.com>
2021-01-15 19:30:02 +0900

    finish 090 配列2

--- 100_様々なオブジェクト/090_Array（配列）２/start/main.js
+++ 100_様々なオブジェクト/090_Array（配列）２/start/main.js
@@ -1,1 +1,16 @@
 const arry = [1, 2, 3, 4, 5];
+
+arry.forEach(function (v, i, arry) {
+  console.log(v);
+})
+
+const newArry = arry.map(function (v, i, arry) {
+  console.log(v);
+  return v * 2;
+})
+console.log(arry, newArry);
+
+const filterArry = arry.filter(function (v, i, arry) {
+  return i === 2; //returnの値がtruesyの時だけreturnされる
+})
+console.log(arry, filterArry); //filterArryにはインデックス番号が2のもののみが格納される
