8f0ad1afa3b86b1b92993f537ad9f6bb8badb1c0
komaitaira <komaitaira@gmail.com>
2021-01-15 18:37:46 +0900

    finish 060 JSON

--- 100_様々なオブジェクト/060_JSON/start/main.js
+++ 100_様々なオブジェクト/060_JSON/start/main.js
@@ -1,1 +1,21 @@
 const obj = {a: 0, b: 1, c: 2};
+
+function replacer(prop, val) {
+  if (val < 1) {
+    return;
+  }
+  return val;
+}
+
+// 【JSON.stringify】
+// ⇨オブジェクトからjsonを作成する。オブジェクトのままではサーバーにデータを渡すということはできないので、jsonに変換してから渡す。
+// const json = JSON.stringify(obj, replacer);
+const json = JSON.stringify(obj, ["a", "b"]);
+console.log(typeof json);
+console.log(json);
+
+// 【JSON.parse】
+// ⇨jsonからオブジェクトを作成する
+const obj2 = JSON.parse(json);
+console.log(typeof obj2);
+console.log(obj2);
